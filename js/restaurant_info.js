fca9d19b1a6cbd5fc5e1b4cc565c3e8baed6998d
Tomasz Chruściński <tom.chruscinski@gmail.com>
2018-08-08 00:59:30 +0200

    Initial commit, new repo stage-2

---
+++ js/restaurant_info.js
@@ -0,0 +1,198 @@
+let restaurant;
+var map;
+
+/**
+ * Initialize Google map, called from HTML.
+ */
+window.initMap = () => {
+  fetchRestaurantFromURL((error, restaurant) => {
+    if (error) { // Got an error!
+      console.error(error);
+    } else {
+      self.map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: restaurant.latlng,
+        scrollwheel: false
+      });
+      fillBreadcrumb();
+      DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
+    }
+  });
+}
+
+/**
+ * Get current restaurant from page URL.
+ */
+fetchRestaurantFromURL = (callback) => {
+  if (self.restaurant) { // restaurant already fetched!
+    callback(null, self.restaurant)
+    return;
+  }
+  const id = getParameterByName('id');
+  if (!id) { // no id found in URL
+    error = 'No restaurant id in URL'
+    callback(error, null);
+  } else {
+    DBHelper.fetchRestaurantById(id, (error, restaurant) => {
+      self.restaurant = restaurant;
+      if (!restaurant) {
+        console.error(error);
+        return;
+      }
+      fillRestaurantHTML();
+      callback(null, restaurant)
+    });
+  }
+}
+
+/**
+ * Create restaurant HTML and add it to the webpage
+ */
+fillRestaurantHTML = (restaurant = self.restaurant) => {
+  const name = document.getElementById('restaurant-name');
+  name.innerHTML = restaurant.name;
+
+  const address = document.getElementById('restaurant-address');
+  address.innerHTML = restaurant.address;
+
+  const picture = document.querySelector('picture');
+  const source1 = document.getElementById('source1');
+  const source2 = document.getElementById('source2');
+  const source3 = document.getElementById('source3');
+  const image = document.getElementById('restaurant-img');
+  image.className = 'restaurant-img'
+
+  source1.srcset = DBHelper.imageUrlForRestaurantlargex2(restaurant) + ' 2x, '+ DBHelper.imageUrlForRestaurantlargex1(restaurant) + ' 1x';
+  source2.srcset = DBHelper.imageUrlForRestaurantlargex2(restaurant);
+  source3.srcset = DBHelper.imageUrlForRestaurantmedium(restaurant);
+  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = 'Photo taken in ' + restaurant.name + ' restaurant.';
+
+  const cuisine = document.getElementById('restaurant-cuisine');
+  cuisine.innerHTML = restaurant.cuisine_type;
+
+  // fill operating hours
+  if (restaurant.operating_hours) {
+    fillRestaurantHoursHTML();
+  }
+  // fill reviews
+  fillReviewsHTML();
+}
+
+/**
+ * Create restaurant operating hours HTML table and add it to the webpage.
+ */
+fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => {
+  const hours = document.getElementById('restaurant-hours');
+  for (let key in operatingHours) {
+    const row = document.createElement('tr');
+
+    const day = document.createElement('td');
+    day.innerHTML = key;
+    row.appendChild(day);
+
+    const time = document.createElement('td');
+    time.innerHTML = operatingHours[key];
+    row.appendChild(time);
+
+    hours.appendChild(row);
+  }
+}
+
+/**
+ * Create all reviews HTML and add them to the webpage.
+ */
+fillReviewsHTML = (reviews = self.restaurant.reviews) => {
+  const container = document.getElementById('reviews-container');
+  const title = document.createElement('h2');
+  title.innerHTML = 'Reviews';
+  container.appendChild(title);
+
+  if (!reviews) {
+    const noReviews = document.createElement('p');
+    noReviews.innerHTML = 'No reviews yet!';
+    container.appendChild(noReviews);
+    return;
+  }
+  const ul = document.getElementById('reviews-list');
+  reviews.forEach(review => {
+    ul.appendChild(createReviewHTML(review));
+  });
+  container.appendChild(ul);
+}
+
+/**
+ * Create review HTML and add it to the webpage.
+ */
+createReviewHTML = (review) => {
+  const li = document.createElement('li');
+  const name = document.createElement('p');
+  const hr1 = document.createElement('hr');
+  const hr2 = document.createElement('hr');
+  const hr3 = document.createElement('hr');
+  hr1.align = 'left';
+  hr2.align = 'left';
+  hr3.align = 'left';
+
+  name.innerHTML = review.name;
+  li.appendChild(name);
+  li.appendChild(hr1);
+
+  const date = document.createElement('p');
+  date.innerHTML = review.date;
+  li.appendChild(date);
+  li.appendChild(hr2);
+
+  const rating = document.createElement('p');
+  rating.innerHTML = `Rating: ${review.rating}`;
+  li.appendChild(rating);
+  li.appendChild(hr3);
+
+  const comments = document.createElement('p');
+  comments.innerHTML = review.comments;
+  li.appendChild(comments);
+
+  return li;
+}
+
+/**
+ * Add restaurant name to the breadcrumb navigation menu
+ */
+fillBreadcrumb = (restaurant=self.restaurant) => {
+  const breadcrumb = document.getElementById('breadcrumb');
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+
+  a.innerHTML = restaurant.name;
+  a.href = '#';
+  a.setAttribute('aria-label', 'Refresh the current page.');
+  a.setAttribute('aria-current', 'page');
+  li.append(a);
+  breadcrumb.appendChild(li);
+}
+
+/**
+ * Get a parameter by name from page URL.
+ */
+getParameterByName = (name, url) => {
+  if (!url)
+    url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
+    results = regex.exec(url);
+  if (!results)
+    return null;
+  if (!results[2])
+    return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+
+/**
+ * Registering the serviceworker
+ */
+
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('sw.js')
+      .then(() => console.log('Service worker successfully registered.'))
+}
---
+++ sw.js
@@ -0,0 +1,149 @@
+if (typeof idb === 'undefined') {
+        self.importScripts('./idb.js');
+    }
+
+var staticCacheName = 'restaurant-app1';
+
+
+const dbPromise = idb.open('restaurants-db', 1, function(upgradeDb) {
+  switch (upgradeDb.oldVersion) {
+    case 0:
+      upgradeDb.createObjectStore('restaurants', {
+        keyPath: 'id'
+      });
+  }
+});
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        'js/main.js',
+        'js/dbhelper.js',
+        'js/restaurant_info.js',
+        'css/styles.css',
+        'images_largex1/1.jpg',
+        'images_largex1/2.jpg',
+        'images_largex1/3.jpg',
+        'images_largex1/4.jpg',
+        'images_largex1/5.jpg',
+        'images_largex1/6.jpg',
+        'images_largex1/7.jpg',
+        'images_largex1/8.jpg',
+        'images_largex1/9.jpg',
+        'images_largex1/10.jpg',
+        'images_largex2/1.jpg',
+        'images_largex2/2.jpg',
+        'images_largex2/3.jpg',
+        'images_largex2/4.jpg',
+        'images_largex2/5.jpg',
+        'images_largex2/6.jpg',
+        'images_largex2/7.jpg',
+        'images_largex2/8.jpg',
+        'images_largex2/9.jpg',
+        'images_largex2/10.jpg',
+        'images_medium/1.jpg',
+        'images_medium/2.jpg',
+        'images_medium/3.jpg',
+        'images_medium/4.jpg',
+        'images_medium/5.jpg',
+        'images_medium/6.jpg',
+        'images_medium/7.jpg',
+        'images_medium/8.jpg',
+        'images_medium/9.jpg',
+        'images_medium/10.jpg',
+        'images_small/1.jpg',
+        'images_small/2.jpg',
+        'images_small/3.jpg',
+        'images_small/4.jpg',
+        'images_small/5.jpg',
+        'images_small/6.jpg',
+        'images_small/7.jpg',
+        'images_small/8.jpg',
+        'images_small/9.jpg',
+        'images_small/10.jpg',
+      ]);
+    })
+
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all(
+      cacheNames.filter(function(cacheName){
+          return cacheName.startsWith('restaurant-') && cacheName != staticCacheName;
+        }).map(function(cacheName){
+          return cache.delete(cacheName);
+        })
+    );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  
+  let requestToCache = event.request;
+  if (event.request.url.indexOf('restaurant.html') > -1) {
+    requestToCache = new Request('restaurant.html');
+  }
+  const Url = new URL(event.request.url);
+  if (Url.port === '1337') {
+    //splits the url into parts on /
+    const parts = Url.pathname.split("/");
+    //if restaurant exists within the url id is the last part of the url => index is length - 1
+    const id = parts[parts.length - 1] === "restaurants" ? "-1" : parts[parts.length - 1];
+    serverEvent(event, id);
+  } else {
+    defaultEvent(event, requestToCache);
+  }
+});
+
+
+    //put stuff into the IndexedDB
+function serverEvent(event, id) {
+
+  event.respondWith(dbPromise.then(function(db) {
+    return db.transaction("restaurants").objectStore("restaurants").get(id);
+    }).then(function (data) {
+      return data && data.data || fetch(event.request).then(function(response) {
+        return response.json();
+      }).then(function(data) {
+        return dbPromise.then(function(db) {
+          var tx = db.transaction("restaurants", "readwrite");
+          tx.objectStore("restaurants").put({
+            id: id,
+            data: data
+          });
+          return data;
+        });
+      });
+    }).then(finalResponse => {
+      return new Response(JSON.stringify(finalResponse));
+    })
+    .catch(error => {
+      return new Response("Error fetching data", {
+        status: 500
+      });
+    })
+  );
+};
+
+
+function defaultEvent(event, requestToCache) {
+
+  event.respondWith(caches.match(requestToCache).then(function(response) {
+    return response || fetch(event.request).then(function(response) {
+      return caches.open(staticCacheName).then(function(cache) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }));
+};
